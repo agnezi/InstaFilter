bcb9be10c353ef0721e77fc64ce1a432cb2ffce2
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-11-09 19:08:49 -0300

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,25 @@
+////
+// Instafilter
+// Created by: itsjagnezi on 09/11/22
+// Copyright (c) today and beyond
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,16 @@
+////
+// Instafilter
+// Created by: itsjagnezi on 09/11/22
+// Copyright (c) today and beyond
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
